bd4e3ac3e8ae7be95fc1bb90524b36e4000fac93
yolanda0213 <yolanda0213h@gmail.com>
2019-08-14 17:24:40 +0800

    done

---
+++ GridPhotoWall/NewCollectionViewController.swift
@@ -0,0 +1,109 @@
+//
+//  NewCollectionViewController.swift
+//  GridPhotoWall
+//
+//  Created by Yolanda H. on 2019/8/1.
+//  Copyright © 2019 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PhotoCollectionViewCell"
+var imageNames = ["009"]
+var cellId = ""
+class NewCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        let width = (collectionView.bounds.width - 1 * 3) / 4
+        let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout
+        flowLayout?.itemSize = CGSize(width: width, height: width)
+        flowLayout?.estimatedItemSize = .zero
+        
+        for i in 10...37 {
+            imageNames.append("0"+String(i))
+        }
+        for j in 1...8 {
+            imageNames.append("00"+String(j))
+        }
+        
+
+        // Register cell classes
+        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return imageNames.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoCollectionViewCell
+        
+        cell.photoImage.image = UIImage(named: imageNames[indexPath.row])
+        
+        return cell
+    }
+    
+    @IBSegueAction func showDetail(_ coder: NSCoder) -> OnePhotoViewController? {
+        let controller = OnePhotoViewController(coder: coder)
+        let cellId = self.collectionView.indexPathsForSelectedItems?.first?.row
+        controller?.imageName = imageNames[cellId!]
+        
+        return controller
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
---
+++ GridPhotoWall/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  GridPhotoWall
+//
+//  Created by Yolanda H. on 2019/8/1.
+//  Copyright © 2019 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    
+}
